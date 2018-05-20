cfbdf2ba99b35e03857ca1156cbf3c714c203cad
codeauslander <cesar.gusto.catano@gmail.com>
2018-05-20 16:46:32 -0500

    pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,14 +1,10 @@
-class Pagram {
+class Pangram {
   constructor(sentence) {
-    this.sentence = sentence;
+    this.sentence = sentence.replace(/ /g,'').toLowerCase().split("");
     this.alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   }
-  isPangram(sentence = this.sentence) {
-    if (sentence === "") {
-      return false;
-    } 
-    sentence = sentence.replace(/ /g,'').toLowerCase().split("");
-    return this.alphabet.every(letter => sentence.indexOf(letter) > -1);
+  isPangram() {
+    return this.sentence.length === 0 ? false : this.alphabet.every(letter => this.sentence.indexOf(letter) > -1);
   }
 }
-module.exports = Pagram;
+module.exports = Pangram;
