181636c0032032c7c91ae756bfe323682b0197d3
codeauslander <cesar.gusto.catano@gmail.com>
2018-06-06 20:14:50 -0500

    anagram

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,28 +1,12 @@
 class Anagram
-  def initialize(word)
-    @anagram = word.downcase.split("")
-    p @anagram
+  def initialize(anagram)
+    @anagram = anagram.downcase 
   end
   def match(word_list)
-    
-    p word_list.select{|word| anagram? word } 
-    word_list.select{|word| anagram? word } 
-
-  end
-  def anagram?(word)
-    return false if word.uniq.length != @anagram.uniq.length
-    word = word.downcase.split("")
-
-    count = 0
-    while  count < word.length
-      return false unless word.include? @anagram[index]
-      word.delete
-    end
-    
-    return true
+    anagram = @anagram.split("").sort
+    word_list.select { |word|  @anagram != word.downcase && anagram  == word.downcase.split("").sort }
   end
 end
-
 module BookKeeping
   VERSION = 2
 end
