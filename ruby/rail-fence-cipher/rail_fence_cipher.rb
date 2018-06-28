8b0cac8fd60d0ac0aefc57bb63ca6f8b947394ce
codeauslander <cesar.gusto.catano@gmail.com>
2018-06-27 21:47:01 -0500

    WIP rail-fence-cipher.rb

--- ruby/rail-fence-cipher/rail_fence_cipher.rb
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -4,16 +4,29 @@ class RailFenceCipher
  def self.encode(text,fences)
   rail_fence_cipher = ""
   index = 0
+  start = index
   count = 0
-  (text.length * fences).times do 
-    if count < fences
-      puts "#{rail_fence_cipher} - #{text[index]} - #{count} - #{fences}"
+  text_length = text.length
+  size = text_length * fences
+
+
+  loop do 
+    puts "#{count}"
+    if count % fences == 0 && index < text_length
+      puts "index: #{index} - #{text[index]} - count: #{count} - fences: #{fences} - size: #{size}"
       rail_fence_cipher += text[index]
-      index += 1
+      count = 0
     end
     count += 1
-    count = 0 if count == fences + 1
+    index += 1 
+    if index == text_length
+      index = start + 1
+      start += 1
+    end
+    break if start == fences
   end
+
+  puts "index: #{index} - #{text[index]} - count: #{count} - fences: #{fences} - size: #{size}" 
   rail_fence_cipher
  end
 
@@ -24,3 +37,5 @@ end
 module BookKeeping
   VERSION = 4
 end
+
+p RailFenceCipher.encode('12345678', 2)
