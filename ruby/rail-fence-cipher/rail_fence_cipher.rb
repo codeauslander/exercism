ec828884d7f8337231204d4eb1a99c7e5909d50e
codeauslander <cesar.gusto.catano@gmail.com>
2018-06-30 16:53:01 -0500

    rail_fence_cipher.rb finished, added fences_sizes and decode method

--- ruby/rail-fence-cipher/rail_fence_cipher.rb
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -1,61 +1,143 @@
 
 class RailFenceCipher
+  VERSION = 1
+  def self.encode(text,number)
+    return text if number == 1 ||
+                 number > text.length
 
- def self.encode(text,number)
-  return text if number == 1
-  text = text.gsub(/[^a-zA-Z]/, '')
-  p text
+    text = text.gsub(/[^a-zA-Z]/, '')
 
-
-  fences = {}
-  index = 1
-  number.times do 
-    fences[index] = []
-    index += 1
-  end
-  
-  index = 1
-  letter = 0
-  direction = true
-  text.length.times do
-    puts "#{index}"
-    fences[index] << text[letter] 
-    letter += 1
-
-    index -= 1 unless direction
-    index += 1 if direction
-    
-    if index == number
-      direction = !direction
-      index = number
-    end
-    if index == 1
-      direction = !direction
-      index = 1
+    fences = {}
+    index = 1
+    number.times do 
+      fences[index] = []
+      index += 1
     end
 
+    index = 1
+    letter = 0
+    direction = true
+    text.length.times do
+      fences[index] << text[letter] 
+      letter += 1
+
+      index -= 1 unless direction
+      index += 1 if direction
+      
+      if index == number
+        direction = !direction
+        index = number
+      end
+      if index == 1
+        direction = !direction
+        index = 1
+      end
+
+    end
+
+    result = ""
+    index = 1
+    number.times do 
+      result += fences[index].join
+      index += 1
+    end
+
+    result          
   end
 
-  result = ""
-  index = 1
-  number.times do 
-    # p fences[index].join
-    result += fences[index].join
-    index += 1
+  def self.fences_sizes(text,number)
+    return text.length if number == 1 ||
+                   number > text.length
+
+    text = text.gsub(/[^a-zA-Z]/, '')
+
+    fences = {}
+    index = 1
+    number.times do 
+      fences[index] = []
+      index += 1
+    end
+
+    index = 1
+    letter = 0
+    direction = true
+    text.length.times do
+      fences[index] << text[letter] 
+      letter += 1
+
+      index -= 1 unless direction
+      index += 1 if direction
+      
+      if index == number
+        direction = !direction
+        index = number
+      end
+      if index == 1
+        direction = !direction
+        index = 1
+      end
+    end
+
+    index = 1
+    sizes = []
+    number.times do
+      sizes << fences[index].length
+      index += 1
+    end
+
+    sizes
   end
 
-  result
- end
+  def self.decode(text,number)
+    return text if number == 1 ||
+                   number > text.length
 
+    text = text.gsub(/[^a-zA-Z]/, '')
 
+    fences = {}
+    index = 1
+    number.times do
+      fences[index] = []
+      index += 1
+    end
+
+    sizes = self.fences_sizes(text, number)
+
+    index = 1
+    letter = 0
+    count = 0
+    text.length.times do
+      fences[index] << text[letter] 
+      letter += 1
+      count += 1
+      if sizes[index - 1] == count 
+        index += 1
+        count = 0
+      end
+    end
+
+    result = ""
+    index = 1
+    direction = true
+    text.length.times do 
+
+      result += fences[index].shift 
+
+      index -= 1 unless direction
+      index += 1 if direction
+      
+      if index == number
+        direction = !direction
+        index = number
+
+      end
+      if index == 1
+        direction = !direction
+        index = 1
+      end
+
+    end
+    result
+  end
 
 end
-
-
-
-# p RailFenceCipher.encode('', 4)
-# p RailFenceCipher.encode('12345678', 2)
-# p RailFenceCipher.encode('One rail, only one rail', 1)
-
-# p RailFenceCipher.encode('WEAREDISCOVEREDFLEEATONCE', 3)
-# p 'WECRLTEERDSOEEFEAOCAIVDEN'
